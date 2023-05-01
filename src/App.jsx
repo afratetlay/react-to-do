282462f9896003573d245429d7720f8f291a496b
afratetlay <afratetlay@gmail.com>
2023-05-01 14:17:41 +0200

    working on the toggle for the checked box

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,19 @@ export default function App() {
         { id: crypto.randomUUID(), title: newItem, completed: false },
       ];
     });
+
+    setNewItem("");
+  }
+
+  function toggleTodo(id, completed) {
+    setToDos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
   }
   return (
     <>
@@ -35,9 +48,13 @@ export default function App() {
       <ul className="list">
         {todos.map((todo) => {
           return (
-            <li>
+            <li key={todo.id}>
               <label>
-                <input type="checkbox" checked={todo.completed} />
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                />
                 {todo.title}
               </label>
               <button className="btn btn-danger">Delete</button>
